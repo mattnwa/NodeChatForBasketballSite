e5ec8f058c7ba02f37955a28ac22aad1fcc100e4
Matt Luker <matt.luker001@gmail.com>
2016-01-19 14:12:29 -0600

    Finished base chat feature

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-var express = require('express');
-var io = require('socket.io').listen(app.listen(port));
+var express = require("express");
 var app = express();
-var port = 3700;
+var port = 3707;
 
+/*app.get("/", function(req, res){
+    res.send("It works!");
+});*/
+
+var io = require('socket.io').listen(app.listen(port));
+app.use(express.static(__dirname + '/public'));
+console.log("Listening on port " + port);
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
-
-app.use(express.static(__dirname + '/public'));
-
-
-
-app.get("/", function(req, res) {
-  res.render("page");
+app.get("/", function(req, res){
+    res.render("page");
 });
-
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
 });
-
-
-var io = require('socket.io').listen(app.listen(port));
-console.log("Listening on port " + port);
