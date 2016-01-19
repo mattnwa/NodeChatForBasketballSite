77989f16cd4a006b31eb746ba25fd71297245358
Matt Luker <matt.luker001@gmail.com>
2016-01-19 02:35:34 -0600

    working on socket.io .listen

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var io = require('socket.io').listen(app.listen(port));
 var app = express();
 var port = 3700;
+
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+
+app.use(express.static(__dirname + '/public'));
 
 
 
@@ -11,5 +15,13 @@ app.get("/", function(req, res) {
   res.render("page");
 });
 
-app.listen(port);
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+
+var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
